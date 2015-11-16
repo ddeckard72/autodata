deafa0ae99df665a9587f252a4fefd3610300a5a
Don Deckard <don_deckard@yahoo.com>
2015-11-16 00:06:16 -0500

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116040935) do
+ActiveRecord::Schema.define(version: 20151116050426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,17 @@ ActiveRecord::Schema.define(version: 20151116040935) do
     t.string   "model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "access_token"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
